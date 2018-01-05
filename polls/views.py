b5158bae12d9dfb30c7f3260065106f54c78c2a9
dunix15 <dunix15@gmail.com>
2018-01-05 12:13:04 +0100

    http response

--- polls/views.py
+++ polls/views.py
@@ -2,15 +2,14 @@ import json
 import requests
 
 from django.http import HttpResponse, HttpResponseForbidden
-from django.utils.decorators import method_decorator
 from django.views.generic import View
-from django.views.decorators.csrf import csrf_exempt
 
 from bot.settings import ACCESS_TOKEN, VERIFY_TOKEN
 
 
 def reply(user_id, msg):
     data = {
+        'messaging_type': 'RESPONSE',
         'recipient': {'id': user_id},
         'message': {'text': msg},
     }
@@ -27,23 +26,18 @@ class HandleMessageView(View):
 
         return HttpResponse('Bot is watching you!')
 
-
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return View.dispatch(self, request, *args, **kwargs)
-
     # Post function to handle Facebook messages
     def post(self, request, *args, **kwargs):
-        # Converts the text payload into a python dictionary
         incoming_message = json.loads(self.request.body.decode('utf-8'))
         # Facebook recommends going through every entry since they might send
         # multiple messages in a single call during high load
-        for entry in incoming_message['entry']:
-            for message in entry['messaging']:
-                # Check to make sure the received call is a message call
-                # This might be delivery, optin, postback for other events
-                if 'message' in message:
-                    # Print the message to the terminal
-                    reply(message['sender']['id'], message['message']['text'])
+        if incoming_message['object'] == 'page':
+            for entry in incoming_message['entry']:
+                for message in entry['messaging']:
+                    # Check to make sure the received call is a message call
+                    # This might be delivery, optin, postback for other events
+                    if 'message' in message:
+                        # Print the message to the terminal
+                        reply(message['sender']['id'], message['message']['text'])
 
-        return HttpResponse()
+        return HttpResponse('OK')
